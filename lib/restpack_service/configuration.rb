1a9dce1887fa43c8201cf0363a91135c481e676d
Gavin Joyce <gavinjoyce@gmail.com>
2013-08-23 16:25:01 +0100

    removed db_table_prefix, convention is better

--- lib/restpack_service/configuration.rb
+++ lib/restpack_service/configuration.rb
@@ -1,14 +1,9 @@
 module RestPack::Service
   class Configuration
-    attr_accessor :db_table_prefix, :application_id
+    attr_accessor :application_id
 
     def initialize
-      @db_table_prefix = 'restpack_'
       @application_id = 1
-    end
-
-    def prefix_db_table(db_table_name)
-      "#{@db_table_prefix}#{db_table_name}".to_sym
     end
   end
 end
--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -4,12 +4,6 @@ describe RestPack::Service::Configuration do
   subject { RestPack::Service::Configuration.new }
 
   it "has defaults" do
-    subject.db_table_prefix.should == "restpack_"
     subject.application_id.should == 1
-  end
-
-  describe ".prefix_db_table" do
-    before { subject.db_table_prefix = 'prefix_' }
-    it { subject.prefix_db_table('table').should == :prefix_table }
   end
 end
