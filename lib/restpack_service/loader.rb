3402458e6ed39a00aed848f5bb0fa31ce038e3de
Gavin Joyce <gavinjoyce@gmail.com>
2013-09-21 18:30:06 +0100

    service loader no longer needs calling directory as an argument

--- lib/restpack_service/loader.rb
+++ lib/restpack_service/loader.rb
@@ -1,6 +1,7 @@
 module RestPack::Service
   class Loader
-    def self.load(name, dir)
+
+    def self.load(name)
       require 'require_all'
 
       require 'restpack_serializer'
@@ -8,7 +9,7 @@ module RestPack::Service
 
       restpack_service_name = "restpack_#{name}_service"
       restpack_namespace = "RestPack::#{name.capitalize}"
-      service_path = "#{dir}/lib/#{restpack_service_name}"
+      service_path = self.get_service_path(caller, restpack_service_name)
 
       require "#{service_path}/version"
       require "#{service_path}/configuration"
@@ -19,6 +20,8 @@ module RestPack::Service
       require_service_module 'commands', service_path, restpack_namespace
     end
 
+    private
+
     def self.require_service_module(module_name, service_path, restpack_namespace)
       require_all "#{service_path}/#{module_name.downcase}"
       proxy_module = Module.new
@@ -26,6 +29,18 @@ module RestPack::Service
         include Object.const_get("#{restpack_namespace}::Service::#{module_name.capitalize}")
       end
       Object.const_set module_name.capitalize, proxy_module
+    end
+
+    def self.get_service_path(load_caller, restpack_service_name)
+      #Gets the path of the calling service : http://stackoverflow.com/questions/14772381/get-directory-of-file-that-instantiated-a-class-ruby
+      source_file = nil
+      load_caller.each do |s|
+        if(s =~ /(require|require_relative)/)
+          source_file = File.dirname(File.expand_path(s.split(':')[0]))
+          break
+        end
+      end
+      "#{source_file}/#{restpack_service_name}"
     end
   end
 end
