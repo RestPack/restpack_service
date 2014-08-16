66f569ff9a74e7b2fb8ea3d8533aa35d57a77a6d
Gavin Joyce <gavin.joyce@intercom.io>
2014-08-16 20:12:28 +0100

    updated list command to use new aliases

--- lib/restpack_service/commands/get.rb
+++ lib/restpack_service/commands/get.rb
@@ -1,9 +1,9 @@
-module RestPack::Service
-  module Commands
-    class Get < RestPack::Service::Command
-      def execute
-        get
-      end
-    end
-  end
-end
+# module RestPack::Service
+#   module Commands
+#     class Get < RestPack::Service::Command
+#       def execute
+#         get
+#       end
+#     end
+#   end
+# end
