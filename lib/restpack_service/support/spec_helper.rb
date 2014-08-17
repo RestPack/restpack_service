13d92917b32ee33337f9d56f3b3f52c6a013f79d
Gavin Joyce <gavin.joyce@intercom.io>
2014-08-17 12:11:05 +0100

    using correct migrations path

--- lib/restpack_service/support/spec_helper.rb
+++ lib/restpack_service/support/spec_helper.rb
@@ -11,8 +11,7 @@ def setup
   config = YAML.load_file('./config/database.yml')
   ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || config['test'])
 
-  migrations_path = File.dirname(__FILE__) + "/../db/migrate"
-  migrations = ActiveRecord::Migrator.up(migrations_path)
+  migrations = ActiveRecord::Migrator.up('./db/migrate')
 
   FactoryGirl.find_definitions
 
