43cf907d7bc545aefc2e317f50d6e72681b9e05a
Gavin Joyce <gavin.joyce@intercom.io>
2014-08-17 13:13:47 +0100

    added generic create command

--- lib/restpack_service/commands/create.rb
+++ lib/restpack_service/commands/create.rb
@@ -1,9 +1,11 @@
-# module RestPack::Service
-#   module Commands
-#     class Create < RestPack::Service::Command
-#       def execute
-#         create!
-#       end
-#     end
-#   end
-# end
+module RestPack::Service
+  module Commands
+    class Create < RestPack::Service::Command
+
+      def execute
+        models = self.class.model_class.create!(inputs[self.class.serializer_class.key])
+        self.class.serializer_class.serialize models
+      end
+    end
+  end
+end
